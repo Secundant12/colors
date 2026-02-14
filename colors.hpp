95faa50b4e419d0bbac0b8b48e5ecbd0505fe892
Secundant12 <104767596+Secundant12@users.noreply.github.com>
2026-02-14 19:03:21 +0100

    Update colors.hpp

--- colors.hpp
+++ colors.hpp
@@ -1,87 +1,110 @@
+/* 
+
+			Colors header was created for simple usage of ANCII-keys to change colors and cursor's position in console app;
+			Created for C++ 17 +    
+
+
+																								by $ecund@nt 
+
+*/
+
 #ifndef COLORS_H
 #define COLORS_H
 
-#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <cstdio>
 #include <unistd.h>
 
-#define CLEAR_SCREEN "\033[2J"
+// ANSI color codes as constexpr string literals
+constexpr char CLEAR_SCREEN[] = "\033[2J";
+constexpr char DEFAULT[] = "\033[39m";
+constexpr char RED[] = "\033[91m";
+constexpr char GREEN[] = "\033[92m";
+constexpr char ORANGE[] = "\033[93m";
+constexpr char BLUE[] = "\033[94m";
+constexpr char BLACK[] = "\033[30m";
+constexpr char WHITE[] = "\033[97m";
+constexpr char MAGENTA[] = "\033[95m";
+constexpr char CYAN[] = "\033[96m";
+constexpr char PURPLE[] = "\033[35m";
+constexpr char YELLOW[] = "\033[33m";
+constexpr char DARK_RED[] = "\033[31m";
+constexpr char DARK_GREEN[] = "\033[32m";
+constexpr char DARK_YELLOW[] = "\033[33m";
 
-#define DEFAULT "\033[39m"
+// Background colors
+constexpr char B_BLACK[] = "\033[40m";
+constexpr char B_WHITE[] = "\033[47m";
+constexpr char B_RED[] = "\033[41m";
+constexpr char B_GREEN[] = "\033[42m";
+constexpr char B_YELLOW[] = "\033[43m";
+constexpr char B_BLUE[] = "\033[44m";
+constexpr char B_PURPLE[] = "\033[45m";
+constexpr char B_BLUE_1[] = "\033[46m";
 
-#define RED "\033[91m"
-#define GREEN "\033[92m"
-#define ORANGE "\033[93m"
-#define BLUE "\033[94m"
-#define BLACK "\033[30m"
-#define WHITE "\033[97m"
-#define MAGENTA "\033[95m"
-#define CYAN "\033[96m"
-#define PURPLE "\033[35m"
-#define YELLOW "\033[33m"
-#define DARK_RED "\033[31m"
-#define DARK_GREEN "\033[32m"
-#define DARK_YELLOW "\033[33m"
+constexpr char RESET[] = "\033[0m";
 
-#define B_BLACK "\033[40m"
-#define B_WHITE "\033[47m"
-#define B_RED "\033[41m"
-#define B_GREEN "\033[42m"
-#define B_YELLOW "\033[43m"
-#define B_BLUE "\033[44m"
-#define B_PURPLE "\033[45m"
-#define B_BLUE_1 "\033[46m"
-
-#define RESET "\033[0m"
-
-
-class CURSOR {
+class Cursor {
 public:
-
-	void setANCIIcode (int code) {
-		printf("\033[%dm" , code);
-	}
-	
-    void setCursorPosition(int row, int col) {
-        printf("\033[%d;%dH", row, col);
+    // Use const reference for better performance with strings
+    void setANSICode(int code) const {
+        std::cout << "\033[" << code << "m";
     }
 
-    void setCursorDefault() {
-        printf("\033[0;0H");
+    void setCursorPosition(int row, int col) const {
+        std::cout << "\033[" << row << ";" << col << "H";
     }
 
-    void moveCursorUp(int lines) {
-        printf("\033[%dA", lines);
+    void setCursorDefault() const {
+        std::cout << "\033[0;0H";
     }
 
-    void moveCursorDown(int lines) {
-        printf("\033[%dB", lines);
+    void moveCursorUp(int lines) const {
+        std::cout << "\033[" << lines << "A";
     }
 
-    void moveCursorForward(int cols) {
-        printf("\033[%dC", cols);
+    void moveCursorDown(int lines) const {
+        std::cout << "\033[" << lines << "B";
     }
 
-    void moveCursorBackward(int cols) {
-        printf("\033[%dD", cols);
+    void moveCursorForward(int cols) const {
+        std::cout << "\033[" << cols << "C";
     }
 
-    void saveCursorPosition() {
-        printf("\033[s");
+    void moveCursorBackward(int cols) const {
+        std::cout << "\033[" << cols << "D";
     }
 
-    void restoreCursorPosition() {
-        printf("\033[u");
+    void saveCursorPosition() const {
+        std::cout << "\033[s";
     }
-    
-    void nline () {
-    	printf("\n");
+
+    void restoreCursorPosition() const {
+        std::cout << "\033[u";
     }
-    
-    void pancii (char * code ) {
-    	printf("\%d",code);
+
+    void newline() const {
+        std::cout << '\n';
+    }
+
+    // Overloaded method for printing with color
+    void print(const std::string& text, const char* color = RESET) const {
+        std::cout << color << text << RESET;
+    }
+
+    void println(const std::string& text, const char* color = RESET) const {
+        std::cout << color << text << RESET << '\n';
+    }
+
+    // Removed pancii() as it had syntax error and unclear purpose
+    // If you need to print a single character code, use:
+    void printChar(char code) const {
+        std::cout << code;
     }
 };
 
-CURSOR cursor;
+// Global cursor instance - consider if this should be extern instead
+inline Cursor cursor;
 
-#endif
+#endif // COLORS_H
